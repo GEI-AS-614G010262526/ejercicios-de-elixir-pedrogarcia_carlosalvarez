e29f173c93a36327debf0ed9db6723fed4043582
carlosAlvarez2 <91893995+carlosAlvarez2@users.noreply.github.com>
2025-10-09 18:14:33 +0200

    Eratostenes concurrente

---
+++ 10-eratostenes/concurrente/eratostenesconc/lib/eratostenesconc.ex
@@ -0,0 +1,53 @@
+defmodule Eratostenesconc do
+  @moduledoc """
+  Documentation for `Eratostenesconc`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Eratostenesconc.hello()
+      :world
+
+  """
+
+  defp crear_filtro(primo) do
+    spawn(fn -> filtrar(primo, nil) end)
+  end
+
+  defp filtrar(primo, siguiente) do
+    receive do
+      {:numero, n} ->
+        if rem(n, primo) != 0 do
+          if siguiente do
+            send(siguiente, {:numero, n})
+          else
+            nuevo_filtro = crear_filtro(n)
+            filtrar(primo, nuevo_filtro)
+          end
+        else
+          filtrar(primo, siguiente)
+        end
+
+      :fin ->
+        if siguiente do
+          send(siguiente, :terminar)
+        end
+
+        exit(:normal)
+    end
+  end
+
+  def enviar_nueros(limite, pid) do
+    Enum.each(2..limite, fn n -> send(pid, {numero: n}) end)
+    send(pid, :fin)
+  end
+
+  def ejecutar(limite) do
+    pid = crear_filtro(2)
+    enviar_numeros(limite, pid)
+  end
+
+end
